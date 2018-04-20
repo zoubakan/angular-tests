28e911cb6f88855fee5064d1bd5979146f2ef838
Zoubeir KANOUN <zoubeir.kanoun@gmail.com>
2018-04-20 06:39:25 +0200

    Uses of ngClass

--- src/app/shared/post.ts
+++ src/app/shared/post.ts
@@ -4,10 +4,10 @@ export class Post {
     loveIts: number;
     created_at: Date;
 
-    constructor(title:string, content:string, loveIts:number){
+    constructor(title:string, content:string, loveIts:number, created_at:Date){
         this.title = title;
         this.content = content;
         this.loveIts = loveIts;
-        this.created_at = new Date();
+        this.created_at = created_at;
     }
   }
